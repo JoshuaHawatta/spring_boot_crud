92b5730f4fa5b7e4fff78fb8c2edc7ef71cd5ae3
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-01 16:44:34 -0300

    chore: minor changes at ResponseResult and ResponseResultWithMessage

--- src/main/java/com/joshua/spring_aula/entities/requestsResults/ResponseResult.java
+++ src/main/java/com/joshua/spring_aula/entities/requestsResults/ResponseResult.java
@@ -3,8 +3,8 @@ package com.joshua.spring_aula.entities.requestsResults;
 import com.joshua.spring_aula.entities.requestsResults.abstractions.ResponseSet;
 
 public class ResponseResult<T> extends ResponseSet<T> {
-    public ResponseResult(Integer statusCode, T ignoreResult) {
+    public ResponseResult(Integer statusCode, T result) {
         this.setStatusCode(statusCode);
-        this.setResult(ignoreResult);
+        this.setResult(result);
     }
 }
--- src/main/java/com/joshua/spring_aula/entities/requestsResults/ResponseResultWithMessage.java
+++ src/main/java/com/joshua/spring_aula/entities/requestsResults/ResponseResultWithMessage.java
@@ -3,12 +3,14 @@ package com.joshua.spring_aula.entities.requestsResults;
 import com.joshua.spring_aula.entities.requestsResults.abstractions.ResponseSet;
 
 public class ResponseResultWithMessage<T> extends ResponseSet<T>  {
-    protected String message;
+    private String message;
 
-    public ResponseResultWithMessage(Integer statusCode, T ignoreResult) {
+    public ResponseResultWithMessage(Integer statusCode, T result) {
         this.setStatusCode(statusCode);
-        this.setResult(ignoreResult);
+        this.setResult(result);
     }
+
+    public String getMessage() { return message; }
 
     public void setMessage(String message) { this.message = message; }
 }
