e7239c3e9086dbcc556426d158055cd472e0c695
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-13 01:20:48 -0300

    chore: added age field to UsersDTO

--- src/main/java/com/joshua/spring_aula/DTOs/UsersDTO.java
+++ src/main/java/com/joshua/spring_aula/DTOs/UsersDTO.java
@@ -5,14 +5,15 @@ import com.joshua.spring_aula.models.Users;
 public class UsersDTO {
     private Long id;
     private String name;
-
-    public UsersDTO() {}
+    private Integer age;
 
     public UsersDTO(Users user) {
         this.id = user.getId();
         this.name = user.getName();
+        this.age = user.getAge();
     }
 
+    //GETTERS_AND_SETTERS
     public Long getId() { return id; }
 
     public void setId(Long id) { this.id = id; }
@@ -20,4 +21,8 @@ public class UsersDTO {
     public String getName() { return name; }
 
     public void setName(String name) { this.name = name; }
+
+    public Integer getAge() { return age; }
+
+    public void setAge(Integer age) { this.age = age; }
 }
